017d503c2b5335a5f24db87e3dca56a9b727f2b1
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-23 20:07:54 -0500

    phase3,4 complete: backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,23 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 import sys
+import json
+import certifi
+from pymongo import MongoClient
 
 # Local modules
 from repo_cloner import clone_repo
 from file_scanner import get_code_files, read_and_metadata
 from chunker import split_into_chunks
 from save_jsonl import save_chunks_jsonl
+from embedding import get_embedding
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI, tlsCAFile=certifi.where())
+collection = client["unrepo"]["code_chunks"]
 
 app = FastAPI()
 
@@ -19,6 +27,7 @@ def ping():
     return {"message": "pong"}
 
 def process_repo(repo_url):
+    # Step 1: Clone and chunk
     repo_name = repo_url.rstrip("/").split("/")[-1]
     repo_dir = clone_repo(repo_url)
 
@@ -40,6 +49,30 @@ def process_repo(repo_url):
             })
 
     save_chunks_jsonl(all_chunks)
+
+    # Step 2: Populate DB
+    # FOR NOW I'LL EMPTY THE DB FOR SPACE, so we basically can have only 1 repo
+    collection.delete_many({})
+    
+    # collection.delete_many({"repo_id": repo_name})  # clear only this repo's data
+
+    inserted_count = 0
+    with open("data/chunks.jsonl", "r") as f:
+        for line in f:
+            chunk = json.loads(line)
+            embedding = get_embedding(chunk["content"]).values
+
+            doc = {
+                "repo_id": repo_name,
+                "content": chunk["content"],
+                "filepath": chunk["filepath"],
+                "language": chunk["language"],
+                "embedding": embedding
+            }
+            collection.insert_one(doc)
+            inserted_count += 1
+
+    print(f"✅ Inserted {inserted_count} chunks into MongoDB for repo '{repo_name}'")
 
 # CLI entry
 if __name__ == "__main__":
