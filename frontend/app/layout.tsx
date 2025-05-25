614065645cb773cbba685f569bf52e4e2623355a
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-25 05:07:22 -0500

    frontend,expose status, limit checks: frontend/app/layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+              <Toaster richColors /> {/* you can customize */}
+
         {children}
       </body>
     </html>
