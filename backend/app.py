4785302444fb916d123189a84f25f073103db14b
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2025-05-26 06:18:41 -0500

    origins

--- backend/app.py
+++ backend/app.py
@@ -32,7 +32,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+allow_origins=[
+    "http://localhost:3000",
+    "https://ungithub.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
